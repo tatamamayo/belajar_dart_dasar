58ac42cdbe5cb47735032210923cadfc3cd00dac
tatamamayo <iniautok@gmail.com>
2024-01-16 20:41:31 +0700

    16 Januari 2024

--- bin/null_safety.dart
+++ bin/null_safety.dart
@@ -11,7 +11,28 @@ void main(){
   // konvensi nullable dan non nullable
   String name = 'eko';
   String? nullable = name;
-  print(name);
+ 
+ int? nullablePrice = null;
+ if (nullablePrice != null){
+   int price = nullablePrice;
+ }
 
+  //  ternary operation
+String? tamu;
+String namaTamu = tamu != null ? tamu : 'Guest';
+print(namaTamu); 
+
+  // default value
+  String? guest;
+  guest = 'eko';
+  String guestName = guest ?? 'Guest';
+  print(guestName);
+
+  // konveksi paksa
+  int? nullableNumber;
+  // nullableNumber = 10;
+  int nonNullableNumber = nullableNumber!; //tp klo pakai ini hati2, karena jika ternyata data yang dimaksud bernilai null, maka akan error ketika dijalankan
+
+  // mengakses nullable member
   
 }
